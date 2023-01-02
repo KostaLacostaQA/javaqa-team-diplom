83de532773f491d039695d4c6cf18ba38554acd6
Kosta <dukov423@mail.ru>
2023-01-02 21:04:35 +0500

    add tests

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
@@ -14,5 +15,61 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+
+    @Test
+    public void publishGameTest() {
+        GameStore store = new GameStore();
+        Game expectedGame = new Game("Нетология Баттл Онлайн", "Аркады", store);
+        Game actualGame = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        assertEquals(expectedGame, actualGame);
+
+        //дублирование игры
+        expectedGame = null;
+        actualGame = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        assertEquals(expectedGame, actualGame);
+    }
+
+    //отсутствует метод для возврата времени игрока, невозможно выполнить тест
+    @Test
+    public void addPlayTimeTest() {
+        GameStore store = new GameStore();
+        //новый игрок
+        store.addPlayTime("Константин", 10);
+
+        //игрок есть в базе данных
+        store.addPlayTime("Константин", 5);
+
+        //недопустимость отрицательных часов и 0
+        store.addPlayTime("Константин", -5);
+        store.addPlayTime("Константин", 0);
+    }
+
+    @Test
+    public void getMostPlayerTest() {
+        GameStore store = new GameStore();
+        //если игроков нет
+        String expectedPlayer = null;
+        assertEquals(expectedPlayer, store.getMostPlayer());
+
+        //если есть игроки
+        store.addPlayTime("Константин", 10);
+        store.addPlayTime("Анна", 5);
+        expectedPlayer = "Константин";
+        assertEquals(expectedPlayer, store.getMostPlayer());
+    }
+
+    @Test
+    public void getSumPlayedTimeTest() {
+        GameStore store = new GameStore();
+        //если игроков нет
+        int expectedTime = 0;
+        assertEquals(expectedTime, store.getSumPlayedTime());
+
+        //если есть игроки
+        store.addPlayTime("Константин", 10);
+        store.addPlayTime("Анна", 5);
+        expectedTime = 15;
+        assertEquals(expectedTime, store.getSumPlayedTime());
+    }
 }
--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -19,5 +21,102 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldSumGenreIfMoreGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Нетология Баттл 2 Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 3);
+        player.play(game1, 2);
+
+        int expected = 5;
+        int actual = player.sumGenre(game.getGenre() + game1.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shoutRefreshTimeGamePlayerTest() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+        player.play(game, 2);
+
+        int expected = 5;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shoutThrowRuntimeExceptionIfNoGameTest() {
+        GameStore store = new GameStore();
+//        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.play(game, 3);
+
+        expected = NotFoundException.class;
+        Assertions.assertThrows(NotFoundException.class, () ->
+        {player.play("Нетология Баттл Онлайн", 3)});
+    }
+
+    @Test
+    public void installGameTest() {
+        GameStore store = new GameStore();
+        Game expectedGame = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(expectedGame);
+        Game actualGame = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        assertEquals(expectedGame, actualGame);
+
+        //Повторное добавление игры игроку
+
+        actualGame = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        player.installGame(actualGame);
+        expectedGame = null;
+        assertEquals(expectedGame, actualGame);
+    }
+
+    @Test
+    public void mostPlayerByGenreTest() {
+
+        //Если установленны 2 игры разного жанра
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Нетология Шахматы Онлайн", "Казуальные");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+
+        player.installGame(game1);
+        player.play(game1, 2);
+
+        player.mostPlayerByGenre("Аркады");
+        Game expectedGenre = game;
+        Game actualGenre = player.mostPlayerByGenre("Аркады");
+        assertEquals(expectedGenre, actualGenre);
+
+        //если игры в данном жанре не играли
+
+        Game game3 = store.publishGame("Нетология Гонки Онлайн", "Гонки");
+        player.installGame(game3);
+        player.play(game3, 1);
+
+        expectedGenre = null;
+        actualGenre = player.mostPlayerByGenre("Ужасы");
+        assertEquals(expectedGenre, actualGenre);
+    }
+
+    //отсутствует метод возврата о том в какую игру сколько часов было сыграно,
+    //невозможно выполнить тест
+
 }
